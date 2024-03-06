da0f2c3b1361f719b41d96363a3f2adb6ceb23cb
Jason <devop.jas@gmail.com>
2024-03-06 12:45:03 +0800

    initial navigation bar done

--- iflex-gym/src/App.tsx
+++ iflex-gym/src/App.tsx
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import AboutUs from "./sections/AboutUs";
 import Services from "./sections/Services";
 import Contact from "./sections/Contact";
+import LandingPage from "./sections/LandingPage";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <main>
+      <main className="px-20">
         <Navbar />
+        <LandingPage />
         <AboutUs />
         <Services />
         <Contact />
--- iflex-gym/src/components/Navbar.tsx
+++ iflex-gym/src/components/Navbar.tsx
@@ -1,14 +1,61 @@
 import { HashLink as Link } from "react-router-hash-link";
+import { useState } from "react";
+
+type navOpen = true | false;
 
 export default function Navbar() {
+  const [openNav, setOpenNav] = useState<navOpen>(false);
+
   return (
-    <nav>
-      <ul>
-        <Link to="#home">Home</Link>
-        <Link to="#about">About</Link>
-        <Link to="#services">Services</Link>
-        <Link to="#contact">Contact</Link>
-      </ul>
-    </nav>
+    <>
+      {/* for large screens */}
+      <nav className=" hidden lg:flex justify-between py-4 items-center lg:py-8">
+        <Link to="#home">
+          <span className="lg:text-4xl font-serif font-semibold text-red-600">
+            IFLEX GYM
+          </span>
+        </Link>
+        <ul className="flex flex-row  lg:gap-6">
+          <Link to="#home">Home</Link>
+          <Link to="#about">About</Link>
+          <Link to="#services">Services</Link>
+          <Link to="#contact">Contact</Link>
+        </ul>
+      </nav>
+      {/* for small screens */}
+      <div>
+        {openNav ? (
+          <nav className=" lg:hidden flex items-start flex-col py-4 justify-between lg:py-8">
+            <Link to="#home" className="flex-row flex justify-between w-full">
+              <span className="lg:text-4xl font-serif font-semibold text-red-600">
+                IFLEX GYM
+              </span>
+              <button
+                onClick={() => setOpenNav(openNav === true ? false : true)}
+              >
+                Close
+              </button>
+            </Link>
+            <ul className="flex lg:hidden flex-col  lg:gap-6">
+              <Link to="#home">Home</Link>
+              <Link to="#about">About</Link>
+              <Link to="#services">Services</Link>
+              <Link to="#contact">Contact</Link>
+            </ul>
+          </nav>
+        ) : (
+          <div className="flex justify-between items-center py-4">
+            <span className="lg:text-4xl font-serif font-semibold text-red-600">
+              IFLEX GYM
+            </span>
+            <button
+              onClick={() => setOpenNav(openNav === false ? true : false)}
+            >
+              Open
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
